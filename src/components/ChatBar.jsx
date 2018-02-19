b64736b768702432b61a1002d1fb643a02d7774e
Jerry Kurian <jkurian770@gmail.com>
2018-02-19 03:36:13 -0500

    ChatBar.jsx: Added onChange handlers which change the state of the component to hold the updated values in the input sections. The value attribute is updated as the user types/state changes

--- src/components/ChatBar.jsx
+++ src/components/ChatBar.jsx
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 
 export default class ChatBar extends Component {
+  constructor(props){
+    super(props);
+    this.state = {inputValue: '', username: props.defaultValue}
+  }
   render() {
+    const messageOnChange = (evt) => {
+      this.setState({inputValue: evt.target.value, username: this.state.username})
+    }
+    const nameOnChange = (evt) => {
+      this.setState({inputValue: this.state.inputValue, username: evt.target.value})
+    }
     return (
     <footer className="chatbar">
-        <input className="chatbar-username" placeholder="Your Name (Optional)" />
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+        <input className="chatbar-username" placeholder="Your Name (Optional)" onChange={nameOnChange} value={this.state.username}/>
+        <input className="chatbar-message" placeholder="Type a message and hit ENTER" onChange={messageOnChange} value={this.state.inputValue}/>
     </footer>
     );
   }
